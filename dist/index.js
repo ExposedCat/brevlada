10b8c2034215af6de7d3b9948c3ef83c1de090ca
Artem Prokop <artem13.prokop@gmail.com>
2025-01-10 10:03:11 +0100

    feat: sender

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,17 @@
 import { fetchAccounts } from "./services/accounts.js";
 import { createReceiver } from "./services/receiver.js";
+import { createSender } from "./services/sender.js";
 const accounts = await fetchAccounts();
 for (const account of accounts) {
     console.log("Connecting to", account.receiver.host);
     const receiver = createReceiver(account);
     await receiver.connect();
-    console.log("Account", account.receiver.username, "connected");
+    console.log("Account", account.receiver.username, "connected (Receive)");
     receiver.disconnect();
+    console.log("Connecting to", account.sender.host);
+    const sender = createSender(account);
+    await sender.verify();
+    console.log("Account", account.receiver.username, "connected (Send)");
+    sender.close();
 }
 //# sourceMappingURL=index.js.map
