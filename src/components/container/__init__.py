9a69413dc5420d954ce466dabd72b9146822c0ba
Artem Prokop <artem13.prokop@gmail.com>
2025-07-08 22:22:23 +0200

    chore: refactor sidebar

--- src/components/container/__init__.py
+++ src/components/container/__init__.py
@@ -38,25 +38,7 @@ class ContentContainer(Gtk.Box):
             else:
                 self.append(children)
 
-class Sidebar(Gtk.Box):
 
-    def __init__(self, class_names=None, children=None, **kwargs):
-        super().__init__(orientation=Gtk.Orientation.VERTICAL, **kwargs)
-        self.add_css_class("sidebar")
-
-        if class_names:
-            if isinstance(class_names, str):
-                self.add_css_class(class_names)
-            elif isinstance(class_names, list):
-                for class_name in class_names:
-                    self.add_css_class(class_name)
-
-        if children:
-            if isinstance(children, list):
-                for child in children:
-                    self.append(child)
-            else:
-                self.append(children)
 
 class NavigationList(Gtk.ListBox):
 
